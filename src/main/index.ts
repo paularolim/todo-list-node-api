f7aa8492881efa4e5cf2126b16cf08335015f6d0
Paula Rolim <rolim.paula1999@gmail.com>
2022-10-23 19:49:24 -0300

    feat: add first endpoint

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,20 @@
-console.log('everything works well');
+/* eslint-disable no-console */
+import express, { Request, Response } from 'express';
+import { env } from 'process';
+
+const port = 3000 || env.PORT;
+
+const app = express();
+
+app.get('/api', (request: Request, response: Response) => {
+  console.log('GET /api');
+
+  return response.json({
+    name: 'Todo List',
+    version: 'v1.0.0',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
